9f413baa98bec19dcc497445b21de7395c8e4ac0
kaguaryan <ryankagua@gmail.com>
2025-04-30 11:10:14 +0300

    Add files via upload

---
+++ variableuse.java
@@ -0,0 +1,9 @@
+public class variableuse {
+    public static void main(String[] args) {
+        int number = 7;
+        number(number);
+    }
+    public static void number(int x) {
+        System.out.println(x);
+    }
+}
