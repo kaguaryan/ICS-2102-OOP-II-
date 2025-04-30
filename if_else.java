d5200f3018c9ee481037cc87c52c45545662c315
kaguaryan <ryankagua@gmail.com>
2025-04-30 11:19:00 +0300

    Add files via upload

---
+++ if_else.java
@@ -0,0 +1,11 @@
+public class if_else {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 7) {
+            System.out.println("Number is greater than 7");
+        } else {
+            System.out.println("Number is 7 or less");
+        }
+    }
+}
